c0b50a9e035e3d28d9f611109ceb8d4f0f1d2c99
Vladimir <vladkodoktorov@gmail.com>
2021-03-20 15:06:37 +0000

    Experimenting with delegates

--- Program.cs
+++ Program.cs
@@ -8,8 +8,11 @@ namespace DelegatesDemo
     class Program
     {
         public delegate bool FilterDelegate(Person p);
+        //generic one
+        public static Func<Person, Person> upperBoy = str => new Person {Name = str.Name.ToUpper(), Age = str.Age };
         static void Main(string[] args)
         {
+            List<string> groups = new List<string>() { "Children", "Adults", "Seniors" };
             //Create 4 Person objects
             Person p1 = new Person() { Name = "John", Age = 41 };
             Person p2 = new Person() { Name = "Jane", Age = 69 };
@@ -17,15 +20,51 @@ namespace DelegatesDemo
             Person p4 = new Person() { Name = "Jessie", Age = 25 };
 
             //Create a list of Person objects and fill it
-            List<Person> people = new List<Person>() { p1, p2, p3, p4 };
+            List<Person> list = new List<Person>() { p1, p2, p3, p4 };
 
-            //Invoke DisplayPeople using appropriate delegate
-            DisplayPeople("Children:", people, IsChild);
-            DisplayPeople("Adults:", people, IsAdult);
-            DisplayPeople("Seniors:", people, IsSenior);
 
+            var formattedListWithDelegate = list.Select(upperBoy).ToList();
+            var convertedListUsingLinq = list.Select(x => new Person
+            {
+                Name = x.Name.ToUpper(),
+                Age = x.Age
+            }).ToList();
+
+            //using delegate
+            FilterGroups(groups, formattedListWithDelegate, true);
+            //just to demonstrate it can be achieved by liq as well
+            FilterGroups(groups, convertedListUsingLinq, false);
             Console.Read();
         }
+
+        private static void FilterGroups(List<string> groups, List<Person> formattedListWithDelegate, bool isDelegate)
+        {
+            if (isDelegate)
+            {
+                Console.WriteLine("using delegate");
+            }
+            else
+            {
+                Console.WriteLine("using linq here");
+            }
+
+            foreach (var group in groups)
+            {
+                if (group == "Children")
+                {
+                    DisplayPeople(group, formattedListWithDelegate, IsChild);
+                }
+                if (group == "Adults")
+                {
+                    DisplayPeople(group, formattedListWithDelegate, IsAdult);
+                }
+                if (group == "Seniors")
+                {
+                    DisplayPeople(group, formattedListWithDelegate, IsSenior);
+                }
+            }
+        }
+
         /// <summary>
         /// A method to filter out the people you need
         /// </summary>
